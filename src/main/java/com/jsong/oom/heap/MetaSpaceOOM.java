5d86acaaa283844f45b47be295ebaf3d2e373e8f
user <user>
2020-04-18 20:09:49 +0800

    加入处理

--- src/main/java/com/jsong/oom/heap/MetaSpaceOOM.java
+++ src/main/java/com/jsong/oom/heap/MetaSpaceOOM.java
@@ -7,8 +7,12 @@ import net.sf.cglib.proxy.MethodProxy;
 import java.lang.reflect.Method;
 
 /**
- * Function:方法区内存溢出 1.8之后修改为元数据区
+ * Function:方法区内存溢出
+ * 1.8之后修改为元数据区
  *
+ * CGLIB基于ASM实现。提供比反射更为强大的动态特性。使用CGLIB可以非常方便的实现的动态代理。
+ *
+ * @href https://www.cnblogs.com/shijiaqi1066/p/3429691.html CGLIB学习笔记
  * @author jsong
  *         Date: 29/12/2017 21:34
  * @since JDK 1.8
