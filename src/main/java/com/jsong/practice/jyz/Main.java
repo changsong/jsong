e919a1c5514c72d65a144f9f6f88a63e810f9cc9
changsong <changsong@wumart.com>
2021-07-09 09:43:19 +0800

    更新处理

---
+++ src/main/java/com/jsong/practice/jyz/Main.java
@@ -0,0 +1,34 @@
+package com.jsong.practice.jyz;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Eclipse - > idea
+ * Git
+ *
+ */
+public class Main {
+	public static void main(String[] args){
+		// 1. 读取文本文件
+
+		// 2. 生成List
+		List<Person> personList = Lists.newArrayList();
+
+//		找出四个人中百米谁跑的最快
+		for(Person person:personList){
+
+		}
+
+//		算出四人的百米平均值
+		for(Person person:personList){
+
+		}
+
+//		算出年纪为17岁中人跑得最快的人
+		for(Person person:personList){
+
+		}
+	}
+}
