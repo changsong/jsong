0dc9b946ea1ce7d4106b6056a695b55932aa62bd
changsong <changsong@wumart.com>
2022-03-29 22:33:27 +0800

    增加数据结构

---
+++ src/main/java/com/jsong/chronicle/TestQueue.java
@@ -0,0 +1,19 @@
+package com.jsong.chronicle;
+
+public class TestQueue {
+
+    public static void main(String[] args) {
+        // String basePath = OS.getTarget() + "/getting-started";
+        // try (ChronicleQueue queue = ChronicleQueue.singleBuilder(basePath).build()) {
+        //     ExcerptAppender appender = queue.acquireAppender();
+        //     appender.writeText("TestMessage");
+        //
+        //     ExcerptTailer tailer = queue.createTailer();
+        //     assertEquals("TestMessage", tailer.readText());
+        //
+        //     queue.close();
+        // }
+
+
+    }
+}
---
+++ src/main/java/com/jsong/chronicle/TestWire.java
@@ -0,0 +1,13 @@
+package com.jsong.chronicle;
+
+public class TestWire {
+    public static void main(String[] args) {
+//         Wire wire = new TextWire(bytes);
+// // or
+//         Bytes<ByteBuffer> bytes2 = Bytes.elasticByteBuffer();
+//         Wire wire2 = new BinaryWire(bytes2);
+// // or
+//         Bytes<ByteBuffer> bytes3 = Bytes.elasticByteBuffer();
+//         Wire wire3 = new RawWire(bytes3);
+    }
+}
